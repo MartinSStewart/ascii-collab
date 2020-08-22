d26953f498ba22503bc96a51a816739c9e4659c9
Unknown <MartinSStewart@gmail.com>
2020-08-22 16:27:17 +0200

    Remove old evergreen again

--- src/Evergreen/V1/Geometry/Types.elm
+++
@@ -1,7 +0,0 @@
-module Evergreen.V1.Geometry.Types exposing (..)
-
-type Point2d units coordinates
-    = Point2d 
-    { x : Float
-    , y : Float
-    }
--- src/Evergreen/V1/LocalModel.elm
+++
@@ -1,8 +0,0 @@
-module Evergreen.V1.LocalModel exposing (..)
-
-type LocalModel msg model
-    = LocalModel 
-    { localMsgs : (List msg)
-    , localModel : model
-    , model : model
-    }
