0ed96308ac64a4213e0e69a8f4f8c34825e5d3c4
Unknown <MartinSStewart@gmail.com>
2020-09-19 18:40:47 +0200

    Shader works now

--- src/Shaders.elm
+++ src/Shaders.elm
@@ -37,111 +37,36 @@ varying vec2 vcoord;
 varying vec4 vcolor;
 
 const float kn = 18.0;
-
-//kn : Float
-//kn =
-//    18
-
 const float xn = 0.95047;
-
-//xn : Float
-//xn =
-//    0.95047
-
-const float yn = 1.0;
-
-//yn : Float
-//yn =
-//    1
-
 const float zn = 1.08883;
-
-//zn : Float
-//zn =
-//    1.08883
-
 const float t0 = 4.0 / 29.0;
-
-//t0 : Float
-//t0 =
-//    4 / 29
-
 const float t1 = 6.0 / 29.0;
-
-//t1 : Float
-//t1 =
-//    6 / 29
-
 const float t2 = 3.0 * pow(t1, 2.0);
-
-//t2 : Float
-//t2 =
-//    3 * t1 ^ 2
-
 const float t3 = pow(t1, 3.0);
-
-//t3 : Float
-//t3 =
-//    t1 ^ 3
 
 float lab2xyz( float t ) {
     return t > t1 ? pow(t, 3.0) : t2 * (t - t0);
 }
 
-//lab2xyz : Float -> Float
-//lab2xyz t =
-//    if t > t1 then
-//        t ^ 3
-//
-//    else
-//        t2 * (t - t0)
-
 float xyz2rgb ( float r ) {
     return r <= 0.00304 ? (12.92 * r) : (1.055 * pow(r, 1.0 / 2.4) - 0.055);
 }
 
-//xyz2rgb : Float -> Float
-//xyz2rgb r =
-//    if r <= 0.00304 then
-//        12.92 * r
-//
-//    else
-//        1.055 * r ^ (1 / 2.4) - 0.055
-
 vec3 lab2rgb(float lightness, float labA, float labB ) {
     float startY = (lightness + 16.0) / 116.0;
-//        startY = (lightness + 16) / 116
-    float y = lab2xyz(startY) * yn;
-//        y = startY |> lab2xyz |> (*) yn
+    float y = lab2xyz(startY);
     float x = lab2xyz(startY + (labA / 500.0)) * xn;
-//        x = startY + (labA / 500) |> lab2xyz |> (*) xn
     float z = lab2xyz(startY - (labB / 200.0)) * zn;
-//        z = startY - (labB / 200) |> lab2xyz |> (*) zn
     float r = xyz2rgb((3.2404542 * x) + (-1.5371385 * y) + (-0.4985314 * z));
-//        r = (3.2404542 * x) + (-1.5371385 * y) + (-0.4985314 * z) |> xyz2rgb
     float g = xyz2rgb((-0.969266 * x) + (1.8760108 * y) + (0.041556 * z));
-//        g = (-0.969266 * x) + (1.8760108 * y) + (0.041556 * z) |> xyz2rgb
     float b = xyz2rgb((0.0556434 * x) + (-0.2040259 * y) + (1.0572252 * z));
-//        b = (0.0556434 * x) + (-0.2040259 * y) + (1.0572252 * z) |> xyz2rgb
     return vec3(clamp(r,0.0,1.0), clamp(g,0.0,1.0), clamp(b,0.0,1.0));
-//        Element.rgb (clamp 0 1 r) (clamp 0 1 g) (clamp 0 1 b)
 }
 
 vec3 lch2rgb( float luminance, float chroma, float hue ) {
-    float hueInRadians = 3.14159265 * hue / 360.0;
+    float hueInRadians = hue * (3.14159265 / 180.0);
     return lab2rgb( luminance, cos(hueInRadians) * chroma, sin(hueInRadians) * chroma );
 }
-
-//lch2lab { luminance, chroma, hue } =
-//    let
-//        hueInRadians =
-//            if isNaN hue then
-//                0
-//
-//            else
-//                degrees hue
-//    in
-//    { lightness = luminance, labA = cos hueInRadians * chroma, labB = sin hueInRadians * chroma }
 
 void main () {
     gl_Position = view * vec4(position, 0.0, 1.0);
@@ -152,7 +77,6 @@ void main () {
     float chroma = mod(userIdFloat * 0.4237, 1.0) * 110.0 + 20.0;
     float hue = userIdFloat * 101.93;
     vec3 rgbColor = lch2rgb(luminance, chroma, hue);
-    //vec3 rgbColor = lch2rgb(80.0, 90.0, 250.0);
 
     vcolor = float(userId != -1.0 && showColors == 1.0) * vec4(rgbColor, 1.0);
 }
@@ -166,21 +90,11 @@ userColor userId =
         userIdFloat =
             toFloat (User.rawId userId + 124)
     in
-    lch2lab
+    lch2rgb
         { luminance = userIdFloat * 0.5219 |> Basics.fractionalModBy 1 |> (*) 50 |> (+) 45
         , chroma = userIdFloat * 0.4237 |> Basics.fractionalModBy 1 |> (*) 110 |> (+) 20
         , hue = userIdFloat * 101.93
         }
-        |> lab2rgb
-
-
-
---lch2lab
---    { luminance = 80 --userIdFloat * 0.5219 |> Basics.fractionalModBy 1 |> (*) 50 |> (+) 45
---    , chroma = 90 --userIdFloat * 0.4237 |> Basics.fractionalModBy 1 |> (*) 110 |> (+) 20
---    , hue = 100 --userIdFloat * 101.93
---    }
---    |> lab2rgb
 
 
 lab2rgb : { lightness : Float, labA : Float, labB : Float } -> Element.Color
@@ -190,7 +104,7 @@ lab2rgb { lightness, labA, labB } =
             (lightness + 16) / 116
 
         y =
-            startY |> lab2xyz |> (*) yn
+            lab2xyz startY
 
         x =
             startY + (labA / 500) |> lab2xyz |> (*) xn
@@ -238,11 +152,6 @@ xn =
     0.95047
 
 
-yn : Float
-yn =
-    1
-
-
 zn : Float
 zn =
     1.08883
@@ -268,8 +177,8 @@ t3 =
     t1 ^ 3
 
 
-lch2lab : { luminance : Float, chroma : Float, hue : Float } -> { lightness : Float, labA : Float, labB : Float }
-lch2lab { luminance, chroma, hue } =
+lch2rgb : { luminance : Float, chroma : Float, hue : Float } -> Element.Color
+lch2rgb { luminance, chroma, hue } =
     let
         hueInRadians =
             if isNaN hue then
@@ -278,7 +187,7 @@ lch2lab { luminance, chroma, hue } =
             else
                 degrees hue
     in
-    { lightness = luminance, labA = cos hueInRadians * chroma, labB = sin hueInRadians * chroma }
+    lab2rgb { lightness = luminance, labA = cos hueInRadians * chroma, labB = sin hueInRadians * chroma }
 
 
 fragmentShader : Shader {} { u | texture : Texture } { vcoord : Vec2, vcolor : Vec4 }
